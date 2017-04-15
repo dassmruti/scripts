ebd010bed18ff962ab323810df490c20f76e86d4
Dassmruti <smruti.sagarika06@gmail.com>
2017-04-15 21:38:12 +0530

    assending n desending order

--- Sortassending.rb
+++ Sortassending.rb
@@ -1,20 +1,31 @@
 class Sorting
-  def self.sort(arr)
-  n = arr.size
-    swapped = false
-      (n-1).times do |i|
-      if arr[i] > arr[i+1]
-        arr[i], arr[i+1] = arr[i+1], arr[i]
-        swapped = true
-      end
-      break if not swapped
+
+  def self.sort(arr, sort_order)
+    n = arr.length
+    (n-1).times do |i|
+      order(arr, sort_order)
+    end
+    return arr
   end
-  arr
- end
+
+  def self.order(arr, sort_order)
+    iterations = arr.length - 1
+    iterations.times do |i|
+      if sort_order == 'asc'
+        if arr[i] > arr[i+1]
+          arr[i], arr[i+1] = arr[i+1], arr[i]
+        end
+      elsif sort_order == 'desc'
+        if arr[i] < arr[i+1]
+          arr[i], arr[i+1] = arr[i+1], arr[i]
+        end
+      end
+    end
+  end
 end
 
-puts "So here is the assending array #{Sorting.sort([8,1,3,25,4,7,9])}"
-puts "So here is the dssending array #{Sorting.sort([8,1,3,25,4,7,9]).reverse}"
+a = [100,0,8,1,25,25,3,4,34]
+print "The final asc arr is #{Sorting.sort(a, 'asc')}\n"
+puts  "The final desc arr is #{Sorting.sort(a, 'desc')}"
 
-# >> So here is the assending array [8, 1, 3, 25, 4, 7, 9]
-# >> So here is the assending array [9, 7, 4, 25, 3, 1, 8]
+# >> The final asc arr is [0, 1, 3, 4, 8, 25, 25, 34, 100]The final desc arr is [100, 34, 25, 25, 8, 4, 3, 1, 0]
