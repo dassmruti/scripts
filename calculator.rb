9e80b6e8f283e14142c37d1a9b70555ad1e89cd3
Dassmruti <smruti.sagarika06@gmail.com>
2017-04-22 17:07:23 +0530

    argv program

---
+++ calculator.rb
@@ -0,0 +1,25 @@
+=begin
+ruby calculator.rb + 2 3 #=> 5
+ruby calculator.rb * 2 3 #=> 6
+=end
+require "pry"
+class Calculator
+
+  def self.calculate(args)
+    if args.length < 3
+      return "Please pass atleast 3 arguments followed by a +/-/*"
+    end
+    operator = args[0]
+    binding.pry
+    case operator
+      when "+"
+        return args[1..-1].map(&:to_i).inject(:+)
+      when "*"
+        return args[1..-1].map(&:to_i).inject(:*)
+      else
+        return "Invalid operator"
+    end
+  end
+end
+
+puts Calculator.calculate(ARGV)
